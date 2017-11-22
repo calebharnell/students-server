86ed26c9a1449088b9990715b41fadb28bc60e65
calebharnell <calebharnell@hotmail.com>
2017-11-22 11:33:21 +1000

    add views

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const port = 3000;
 
 let students = ['Scott', 'Simon'];
 
+app.set('view engine', 'pug');
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
-app.get('/students', function (req, res) {
+app.get('/api/students', (req, res) => {
   res.send(students);
 });
 
-app.post('/students', function (req, res) {
+app.get('/students', (req, res) => {
+  res.render('students', {students});
+});
+
+app.post('/students', (req, res) => {
   let student = req.body.name;
   students.push(student);
   res.send(student);
